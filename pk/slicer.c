0b5d2c17ef13a0f1b8b03baa11c01d271303cc3d
MaxXSoft <x@MaxXSoft.net>
2022-03-25 19:02:06 +0800

    (slicer) added module `slicer`

---
+++ pk/slicer.c
@@ -0,0 +1,11 @@
+// See LICENSE for license details.
+
+#include "slicer.h"
+
+size_t checkpoint_interval; // set by -c flag
+const char *checkpoint_dir; // set by -d flag
+
+void slicer_init()
+{
+  //
+}
---
+++ pk/slicer.h
@@ -0,0 +1,13 @@
+// See LICENSE for license details.
+
+#ifndef _PK_SLICER_H
+#define _PK_SLICER_H
+
+#include <stddef.h>
+
+extern size_t checkpoint_interval;
+extern const char *checkpoint_dir;
+
+void slicer_init();
+
+#endif
