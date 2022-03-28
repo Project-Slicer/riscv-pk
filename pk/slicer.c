a5d01b4badcac5ea0f4cd3c9e6a9d7c49489630c
MaxXSoft <x@MaxXSoft.net>
2022-03-28 17:05:46 +0800

    (slicer) added more implementations

--- pk/slicer.c
+++ pk/slicer.c
@@ -2,12 +2,30 @@
 
 #include "slicer.h"
 #include "pk.h"
+#include "file.h"
 #include <stdint.h>
 
 size_t checkpoint_interval; // set by -c flag, milliseconds
-const char *checkpoint_dir; // set by -d flag
+const char* checkpoint_dir; // set by -d flag
+const char* restore_file; // set by -r flag
 
 static uint64_t last_checkpoint_cycle;
+static file_t* syscall_trace_file;
+
+// Traces system calls and dumps them to the trace file.
+static void trace_syscall(const trapframe_t* tf)
+{
+  // TODO
+}
+
+// Performs checkpoint operation.
+static void do_checkpoint(const trapframe_t* tf)
+{
+  // TODO
+  printk("checkpointing not implemented\n");
+  if (checkpoint_dir)
+    printk("  checkpoint directory: %s\n", checkpoint_dir);
+}
 
 void slicer_init()
 {
@@ -22,14 +40,11 @@ void slicer_syscall_handler(const void* tf)
   // skip if checkpointing is disabled
   if (!checkpoint_interval) return;
 
-  uint64_t cycle = rdcycle64();
   kassert(CLOCK_FREQ % 1000 == 0);
-  if ((cycle - last_checkpoint_cycle) / (CLOCK_FREQ / 1000) >= checkpoint_interval) {
-    last_checkpoint_cycle = cycle;
+  if ((rdcycle64() - last_checkpoint_cycle) / (CLOCK_FREQ / 1000) >= checkpoint_interval) {
+    trace_syscall((const trapframe_t*)tf);
+    do_checkpoint((const trapframe_t*)tf);
 
-    // TODO
-    printk("checkpointing not implemented\n");
-    if (checkpoint_dir)
-      printk("  checkpoint directory: %s\n", checkpoint_dir);
+    last_checkpoint_cycle = rdcycle64();
   }
 }
