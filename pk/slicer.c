cb3709064457f7ca3d02e7709b3e8acf759f64ab
MaxXSoft <x@MaxXSoft.net>
2022-05-10 11:22:28 +0800

    (slicer) added more implementations about syscall trace

--- pk/slicer.c
+++ pk/slicer.c
@@ -6,6 +6,7 @@
 #include "flush_icache.h"
 #include "ksyscall.h"
 #include "checkpoint.h"
+#include "dump.h"
 #include <stdint.h>
 #include <errno.h>
 #include <stdbool.h>
@@ -57,17 +58,50 @@ static inline bool should_checkpoint(const trapframe_t* tf)
   return meet_interval;
 }
 
+// Traces system calls and dumps them to the trace file.
+static void trace_syscall(const trapframe_t* tf)
+{
+  strace_t strace;
+  for (size_t i = 0; i < 6; i++)
+    strace.args[i] = tf->gpr[10 + i];
+  strace.args[6] = tf->gpr[17];
+  strace.epc = tf->epc;
+  write_assert(strace_fd, &strace, sizeof(strace));
+}
+
+// Checks for system call trace.
+static bool check_syscall_trace(const trapframe_t* tf)
+{
+  strace_t strace;
+  ssize_t len = sys_read(strace_fd, &strace, sizeof(strace));
+  if (len == 0) {
+    sys_exit(0);
+  } else if (len < 0 || (size_t)len != sizeof(strace)) {
+    return false;
+  } else {
+    for (size_t i = 0; i < 6; i++) {
+      if (strace.args[i] != tf->gpr[10 + i])
+        return false;
+    }
+    if (strace.args[6] != tf->gpr[17] || strace.epc != tf->epc)
+      return false;
+  }
+  return true;
+}
+
 void slicer_syscall_handler(const void* tf)
 {
-  // skip if checkpointing is disabled
-  if (!checkpoint_interval) return;
-
-  // trace system call
-  trace_syscall(strace_fd, tf);
-
-  // perform checkpoint
-  if (should_checkpoint(tf))
-    slicer_checkpoint(tf);
+  if (checkpoint_interval) {
+    // perform checkpoint
+    if (should_checkpoint(tf))
+      slicer_checkpoint(tf);
+    // trace system call
+    trace_syscall(tf);
+  } else if (restore_dir) {
+    // check system call trace
+    if (!check_syscall_trace(tf))
+      panic("system call trace mismatch");
+  }
 }
 
 void slicer_syscall_post_handler(const void* tf)
@@ -89,12 +123,21 @@ void slicer_checkpoint(const void* tf)
 
   // do checkpoint
   do_checkpoint(tf);
+
+  // update system call trace
+  close_assert(strace_fd);
+  ret = sys_renameat(old_dir_fd, "strace", dir_fd, "strace");
+  kassert(ret == 0);
+  strace_fd = openw_assert("strace");
+
+  // update checkpoint cycle counter
   last_checkpoint_cycle = rdcycle64();
 
   // TODO: remove
   panic("checkpointed");
 
   // restore directory
+  close_assert(dir_fd);
   dir_fd = old_dir_fd;
   ++checkpoint_id;
 }
@@ -106,10 +149,12 @@ void slicer_restore(uintptr_t kstack_top)
   if (dir_fd < 0)
     panic("failed to open checkpoint directory: %s", restore_dir);
 
+  // restore from the given checkpoint
   trapframe_t tf;
   do_restore(&tf);
 
-  // TODO: system call trace
+  // open system call trace
+  strace_fd = openr_assert("strace");
 
   // TODO: remove if supports microarchitectural state restoration
   __riscv_flush_icache();
